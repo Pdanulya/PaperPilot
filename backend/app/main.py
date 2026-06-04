54567fb4400c2819af7aab0ef76c9085fb2341c7
Pdanulya <poojanidanulya@gmail.com>
2026-06-04 13:44:06 +0530

    completed user authentication

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes.auth import router as auth_router
 from app.db.database import engine, Base
 from app.models.user import User
+from app.routes.user import router as user_router
 
 # Create tables
 Base.metadata.create_all(bind=engine)
@@ -13,6 +14,7 @@ app = FastAPI(
 )
 
 app.include_router(auth_router)
+app.include_router(user_router)
 
 @app.get("/health")
 def health_check():
