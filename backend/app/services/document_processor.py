2c4b37d965b2ed6b9cddd1dff2d399ddeb1458d5
Pdanulya <poojanidanulya@gmail.com>
2026-06-07 10:01:56 +0530

    added pdf,docx and txt extraction functions

---
+++ backend/app/services/document_processor.py
@@ -0,0 +1,28 @@
+import fitz
+from docx import Document as DocxDocument
+
+def extract_txt_text(file_path: str):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+def extract_docx_text(file_path: str):
+    doc = DocxDocument(file_path)
+
+    text = []
+
+    for paragraph in doc.paragraphs:
+        text.append(paragraph.text)
+
+    return "\n".join(text)
+
+def extract_pdf_text(file_path: str):
+    pdf = fitz.open(file_path)
+
+    text = []
+
+    for page in pdf:
+        text.append(page.get_text())
+
+    pdf.close()
+
+    return "\n".join(text)
