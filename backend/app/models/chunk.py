187e3c0781083be36b71f8f66ea0dabe03655110
Pdanulya <poojanidanulya@gmail.com>
2026-06-08 16:38:30 +0530

    created document_chunks model

---
+++ backend/app/models/chunk.py
@@ -0,0 +1,32 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    ForeignKey,
+    Text
+)
+from app.db.database import Base
+
+class DocumentChunk(Base):
+    __tablename__ = "document_chunks"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    document_id = Column(
+        Integer,
+        ForeignKey("documents.id"),
+        nullable=False
+    )
+
+    chunk_index = Column(
+        Integer,
+        nullable=False
+    )
+
+    content = Column(
+        Text,
+        nullable=False
+    )
