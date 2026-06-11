3b45e801ce0d5ad21400574fec67f5aeac258616
Pdanulya <poojanidanulya@gmail.com>
2026-06-11 23:07:17 +0530

    commented

--- backend/app/services/retrieval_service.py
+++ backend/app/services/retrieval_service.py
@@ -3,7 +3,7 @@ from sqlalchemy import text
 # This function retrieves the most relevant document chunks based on the provided query embedding.
 def retrieve_relevant_chunks(
     db, # SQLAlchemy database session
-    document_id,
+    document_id, # ID of the document to search within. Only search this doc instead of searching all.
     query_embedding, #vector created from user question
     limit=5 # How many chunks to retrieve.
 ):
