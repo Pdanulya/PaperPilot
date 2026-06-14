e0e6d1b0b97079c32e3f9769eebfad0099df8d67
Pdanulya <poojanidanulya@gmail.com>
2026-06-14 12:46:41 +0530

    solved the error

--- backend/app/models/chat_message.py
+++ backend/app/models/chat_message.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, func
 from sqlalchemy.orm import relationship
-from app.db.base import Base
+from app.db.database import Base
 
 class ChatMessage(Base):
     __tablename__ = "chat_messages"
