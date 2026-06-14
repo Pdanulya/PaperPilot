101ec0bdcf4061ecb443562a89abd58a4608804d
Pdanulya <poojanidanulya@gmail.com>
2026-06-15 01:09:24 +0530

    updated with new models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,5 @@ from .user import User
 from .document import Document
 from .chunk import DocumentChunk
 from .chat_message import ChatMessage
+from .saved_document import SavedDocument
+from .document_view import DocumentView
